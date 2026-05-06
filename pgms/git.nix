81e0286e4859cc065a12e62aac32ac857f5c9b21
Sander Hageman <sander.hageman@channable.com>
2026-05-06 13:36:23 +0200

    add git fixup command

--- pgms/git.nix
+++ pgms/git.nix
@@ -7,6 +7,10 @@
         name = "Sander";
         email = "sander@shageman.nl";
       };
+
+      alias = {
+        fixup = "!git log -n 50 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --fixup";
+      };
     };
   };
 
